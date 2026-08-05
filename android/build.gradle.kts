93fbbba5ac54ac4e82aee457f42b5458bc90d73e
Lucas Bonomo <bonomo.lucas@gmail.com>
2026-08-04 23:35:45 -0300

    Android version

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -5,6 +5,17 @@ allprojects {
     }
 }
 
+// Force all Android library subprojects to compile against SDK 36 so that
+// transitive dependencies (e.g. desktop_drop) that ship with compileSdk=33
+// don't block the AAR metadata check.
+subprojects {
+    afterEvaluate {
+        extensions.findByType(com.android.build.gradle.LibraryExtension::class.java)?.let {
+            it.compileSdk = 36
+        }
+    }
+}
+
 val newBuildDir: Directory =
     rootProject.layout.buildDirectory
         .dir("../../build")
